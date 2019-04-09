925fc0171d768f2241f36b85d2310ec37934a3e8
Ky Ho <kyho.hcmut@gmail.com>
2019-04-09 07:37:54 +0700

    seeding for database (spring java)

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -12,16 +12,26 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        // Let's truncate our existing records to start from scratch.
-        Article::truncate();
+//        // Let's truncate our existing records to start from scratch.
+//        Article::truncate();
+//
+//        $faker = \Faker\Factory::create();
+//
+//        // And now, let's create a few articles in our database:
+//        for ($i = 0; $i < 50; $i++) {
+//            Article::create([
+//                'title' => $faker->sentence,
+//                'body' => $faker->paragraph,
+//            ]);
 
+//        }
+        \App\Products::truncate();
         $faker = \Faker\Factory::create();
-
-        // And now, let's create a few articles in our database:
-        for ($i = 0; $i < 50; $i++) {
-            Article::create([
-                'title' => $faker->sentence,
-                'body' => $faker->paragraph,
+        for ($i = 0; $i < 100; $i ++ ){
+            \App\Products::create([
+                'id' => $i,
+               'name' => $faker->name(),
+               'price' => $faker->randomFloat(2,0,1000000)
             ]);
         }
     }
